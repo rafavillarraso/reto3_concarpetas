68ee907fe6bce6b0257a8144ab0b5b1da875a426
Rafa Villarraso <rafavillarrasoboluda@iMac-de-Rafa.local>
2021-08-30 13:00:20 +0200

    se formatea el titulo y su estado en focus. Se añade el paquete de icons de material-ui para usar el icono de las opciones

--- src/components/List/Title.jsx
+++ src/components/List/Title.jsx
@@ -1,17 +1,49 @@
 import React, {useState} from 'react';
-import {Typography} from '@material-ui/core';
+import {Typography, InputBase} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+
+const useStyle = makeStyles((theme)=>({
+    editableTitleContainer: {
+        display: 'flex',
+        margin: theme.spacing(1),
+    },
+    editableTitle: {
+        flexGrow: 1,
+    },
+    input:{
+        margin: theme.spacing(1),
+        "&:focus":{
+            background:'white',
+        },
+    },
+}));
 
 function Title(){
     const [open, setOpen] = useState(false);
+    const classes = useStyle();
+
     return(
         <div>
         {open ? (
             <div>
-                <InpupBase value="Todo" />
+                <InputBase 
+                    value="Todo"
+                    inputProps={{
+                        className: classes.input
+                    }}
+                    fullWidth
+                    onBlur={()=>setOpen(!open)}/>
             </div>
             ) : (
-                <div>
-            <Typography>Todo</Typography>        
+            <div className={classes.editableTitleContainer}>
+            <Typography
+                onClick={()=> setOpen(!open)}
+                className={classes.editableTitle}>
+                    Todo
+            </Typography> 
+            <MoreHorizIcon />       
         </div>
     )}
     </div>  
