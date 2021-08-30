00e08937d4432fcdfd56272adb9ba0cf2a18eeaa
Rafa Villarraso <rafavillarrasoboluda@iMac-de-Rafa.local>
2021-08-30 11:56:33 +0200

    se añaden las carpetas para crear la app de trello y se eliminan los componentes creados para las pruebas

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import UserForm from './components/UserForm/UserForm'
-
-
-// const Saluda = props => {
-//   console.log(props)
-//   return <h3>Hola soy {props.name}, tengo {props.edad} años</h3>;
-
-// }
-
-
-//  (Lo mismo que antes pero con clases:)
-//
-//  class Saluda extends React.Component {
-//     constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     console.log(this.props)
-//     return <h3>Hola soy {this.props.name}, tengo {this.props.edad} años</h3>;
-//   }
- 
-// }
+import React, { Component } from 'react';
 
 
 function App() {
-  const [showCounters,setShowCounters]=useState(true)
-  const quitarCounters =()=> setShowCounters(false)
   return (
     <div className="App">
       <header className="App-header">
-        <UserForm />
-        {/* {showCounters && <React.Fragment>
-          <Counter initialValue={10} step={2}/>
-          <Counter initialValue={5} step={5}/>
-        </React.Fragment>
-        }
-        <button onClick={quitarCounters}>Quitar counter</button> */}
+      <h1>Hola mundo</h1>
       </header>
     </div>
   );
