3ecc36b29e09363cf14da218c840ce64f0cd7025
Rafa Villarraso <rafavillarrasoboluda@iMac-de-Rafa.home>
2021-08-31 16:29:12 +0200

    se añade el código para pasar tareas de entre columnas

--- src/App.js
+++ src/App.js
@@ -73,9 +73,52 @@ const addMoreList = (title) => {
     setData(newState);
   }
 
+
+  const onDragEnd = (result) => {
+    const {destination, source, draggableId} = result;
+    console.log('desnation', destination, 'source',source, draggableId);
+
+    if (!destination) {
+      return;
+    }
+    const sourceList = data.lists[source.droppableId];
+    const destinationList = data.lists[destination.droppableId];
+    const draggingCard = sourceList.cards.filter(
+      (card) => card.id === draggableId
+    )[0];
+
+    if (source.droppableId === destination.droppableId){
+      sourceList.cards.splice(source.index,1);
+      destinationList.cards.splice(destination.index,0,draggingCard);
+      const newState = {
+        ...data,
+        lists: {
+          ...data.lists,
+          [sourceList.id]: destinationList,
+        },
+      };
+      setData(newState);
+    } else {
+      sourceList.cards.splice(source.index,1);
+      destinationList.cards.splice(destination.index, 0, draggingCard);
+
+      const newState = {
+        ...data,
+        lists:{
+          ...data.lists,
+          [sourceList.id]: sourceList,
+          [destinationList.id]: destinationList,
+        },
+      };
+      setData(newState);
+    }
+
+
+  }
+
   return (
     <StoreApi.Provider value={{ addMoreCard, addMoreList, updateListTitle }}>
-        <DragDropContext>
+        <DragDropContext onDragEnd={onDragEnd}>
           <div className={classes.root} >
             {data.listIds.map((listId)=> {
               const list = data.lists[listId];
